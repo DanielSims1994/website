861924771c2eb2a71cb5f8221f2a9d9c9b90634b
danielsims1994 <dannys19940@hotmail.co.uk>
2016-05-13 04:32:44 -0700

    adding index and styling

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+	<head>
+	  	<meta charset="utf-8">
+	  	<title>Daniel Sims</title>
+	  	<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+
+	<ul>
+	  <li><a class="active" href="index.php">Home</a></li>
+	  <li class="sign_up_in"><a href="sign_in.php">Log in!</a></li>
+	  <li class="sign_up_in"><a href="sign_up.php">Sign up!</a></li>
+	</ul>
+</body>
+</html>
+	</body>
+</html>
